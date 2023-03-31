613fe6d3f4d50cbc4e5e632b78722804f3b8eea8
Belochka <herangelicgaze@gmail.com>
2023-03-31 20:43:46 +0300

    Добавьте файлы проекта.

---
+++ kursovik.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+
+class BaseString
+{
+public:
+	char content[100];
+	int length = sizeof(content) / sizeof(content[0]);
+
+	void AddToStart(char c)
+	{
+		for (int i = length - 2; i >= 0; i--)
+		{
+			content[i + 1] = content[i];
+		}
+
+		content[0] = c;
+
+	}
+
+	void AddToEnd(char c)
+	{
+		int i = 0;
+		while (content[i] != '\0')
+		{
+			i++;
+			if (content[i] == '\0')
+			{
+				content[i] = c;
+				content[i + 1] = '\0';
+			}
+		}
+	}
+
+	void ShowString()
+	{
+		std::cout << content << std::endl;
+	}
+
+	BaseString(char arr[])
+	{
+		for (int i = 0; i < length; i++)
+		{
+			content[i] = arr[i];
+		}
+	}
+
+};
+
+int main()
+{
+	char content[100];
+
+	printf("Input:\n");
+	scanf_s("%s", &content, (unsigned)_countof(content));
+
+	BaseString based{ content };
+
+	char c = 'h';
+
+	based.AddToStart(c);
+	printf("Output:\n");
+	based.ShowString();
+}
+
